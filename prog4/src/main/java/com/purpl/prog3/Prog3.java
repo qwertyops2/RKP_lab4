e388edf9cbba8549ef3627951a49917d651eedec
Valerkus <166000151+qwertyops2@users.noreply.github.com>
2026-03-19 14:55:03 +0300

    Add files via upload

---
+++ prog4/src/main/java/com/purpl/prog3/Prog3.java
@@ -0,0 +1,7 @@
+package com.purpl.prog3;
+
+public class Prog3 {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> new JFrameGUI().setVisible(true));
+    }
+}
